a2bb5d239beed55db9ae0ce9b939646350ae0e8a
Ashneel Girivar Bhagoban <ashneel-girivar.bhagoban@epita.fr>
2026-05-12 15:52:29 +0200

    init 2.0

--- delivery_app/models.py
+++ delivery_app/models.py
@@ -1,1 +1,4 @@
 # database schemas (drivers, tasks)
+# This defines what your data looks like. 
+# It tells the app: "A Driver must have a Name and an ID," 
+# and "A Destination must have an Address and a Status."
---
+++ delivery_app/static/script.js
@@ -0,0 +1,4 @@
+// javascript duh
+// Handles interactive stuff that happens without 
+// reloading the page—like showing a popup or updating 
+// a map marker.
