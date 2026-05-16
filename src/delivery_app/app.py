b76c685a34ce7b31ba530bac351521e8e2b1dede
Youssef Kang <youssef.kang@epita.fr>
2026-05-16 10:41:11 +0200

    base html done

---
+++ src/delivery_app/app.py
@@ -0,0 +1,5 @@
+# entry point
+# The most important file. It directs traffic. 
+# When a user clicks a link, 
+# app.py decides which page to show them or what data to grab 
+# from the database.
---
+++ src/delivery_app/config.py
@@ -0,0 +1,3 @@
+# database URLs/secret keys
+# Contains secret settings, like your database password 
+# or "Secret Keys" that keep your login system secure.
